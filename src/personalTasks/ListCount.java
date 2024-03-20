408f0faba95c37b7b86732f8818989ffd7e05a37
Izunajune <izuchukwuijeudo@gmail.com>
2024-03-20 13:28:32 +0100

    update

--- src/personalTasks/ListCount.java
+++ src/personalTasks/ListCount.java
@@ -3,23 +3,39 @@ package personalTasks;
 import java.util.Arrays;
 
 public class ListCount {
-    public static int countList(int[] numbers){
-        int countMostOccured = 0;
-        int occuredCount = 0;
+    public static int[] countList(int[] numbers) {
+        int largest = 0;
 
-        for (int index = 0; index < numbers.length; index++) {
-            if(numbers[index] == numbers[index]){
-                countMostOccured += 1;
+        int[] countMostOccured = new int[2];
+        int[] occuredCount = new int[numbers.length];
+
+        for (int count = 0; count < numbers.length; count++) {
+            for (int numberElement : numbers) {
+                if (numbers[count] == numberElement) {
+                    occuredCount[count] += 1;
+                }
+
             }
         }
-            return countMostOccured;
+
+        for (int index = 0; index < occuredCount.length; index++) {
+            if (occuredCount[index] > largest) {
+                largest = occuredCount[index];
+                countMostOccured[1] = numbers[index];
+                countMostOccured[0] = largest;
+            }
         }
+        return countMostOccured;
+    }
+
+
+
 
     public static void main(String[] args) {
         int[] numbers = {1,2,2,3,4,2};
-        countList(numbers);
+        System.out.println(Arrays.toString(countList(numbers)));
 
-        System.out.println(Arrays.toString(new int[]{countList(numbers)}));
+
 
     }
 
