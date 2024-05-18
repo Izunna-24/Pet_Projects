6bdf841dc04557e89410ef8f27bd090d3ed42a8e
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-05-18 15:58:09 +0100

    update

--- src/Chapter17/functionalInterface/intPredicate/IntPredicateSample.java
+++ src/Chapter17/functionalInterface/intPredicate/IntPredicateSample.java
@@ -9,5 +9,19 @@ public class IntPredicateSample {
         boolean result = intPredicate.test(-1);
         System.out.println("is -1 less than zero? " + result);
 
+
+
+
+
+
+    IntPredicate isPrime = (int number) -> {
+        if (number <= 1) return false;
+        for (int count = 2; count <= Math.sqrt(number); count++) {
+            if(number % count == 0) return false;
+        }
+        return true;
+    };
+
+        System.out.println("is 8 a prime number " + isPrime.test(8));
     }
 }
