4b08be86558f92753175f30627f6ecbe8877ba9b
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-05-18 16:18:57 +0100

    update

--- src/Chapter17/functionalInterface/longFunction/LongFunctionSample.java
+++ src/Chapter17/functionalInterface/longFunction/LongFunctionSample.java
@@ -4,7 +4,7 @@ import java.util.function.LongFunction;
 
 public class LongFunctionSample {
     public static void main(String[] args) {
-        LongFunction<String> longFunction = (long accountNumber) -> Long.toString(accountNumber);
+        LongFunction<String> longFunction = Long::toString;
 
         String result = longFunction.apply(1938478777834743L);
         System.out.println(" String equivalence of the number given is " + result);
